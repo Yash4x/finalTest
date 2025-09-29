4d29e9def91cd20d16e999ad2d4438eee0657629
Yash Mandal <imyashmandal@gmail.com>
2025-09-29 18:00:57 -0400

    imported starter files

---
+++ tests/test_operations.py
@@ -0,0 +1,13 @@
+from src.operations import addition, multiply, subtraction
+
+
+def test_addition():
+    assert addition(2, 3) == 5
+
+
+def test_subtraction():
+    assert subtraction(5, 3) == 2
+
+
+def test_multiply():
+    assert multiply(2, 3) == 6
